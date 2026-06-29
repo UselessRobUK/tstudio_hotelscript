b6cb96f71a859ec3e6363075cc3effcb8b4aeb07
UselessRobUK <124530790+UselessRobUK@users.noreply.github.com>
2026-06-29 16:15:46 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Debug = true
+
+Config.InteractKey = 38
+
+Config.DefaultPayment = "cash"
+
+Config.Hotels = {
+    {
+        id = "main_hotel",
+        name = "Main City Hotel",
+        entrance = vector4(-268.14, -957.24, 31.22, 205.0)
+    }
+}
