358985f06c067ae4a280bb85f86cfd1c9800a761
UselessRobUK <124530790+UselessRobUK@users.noreply.github.com>
2026-06-29 19:50:49 +0100

    Create target.lua

---
+++ client/target.lua
@@ -0,0 +1,121 @@
+--========================================================--
+-- Standalone Hotel System
+-- client/target.lua
+--========================================================--
+
+local UsingOxTarget = GetResourceState("ox_target") == "started"
+local UsingQBTarget = GetResourceState("qb-target") == "started"
+
+local RegisteredZones = {}
+
+--------------------------------------------------
+-- Helpers
+--------------------------------------------------
+
+local function AddTargetZone(data)
+    if UsingOxTarget then
+        return exports.ox_target:addBoxZone(data)
+    end
+
+    if UsingQBTarget then
+        return exports['qb-target']:AddBoxZone(
+            data.name,
+            data.coords,
+            data.size.x,
+            data.size.y,
+            {
+                name = data.name,
+                heading = data.heading or 0.0,
+                debugPoly = false,
+                minZ = data.minZ,
+                maxZ = data.maxZ
+            },
+            {
+                options = data.options,
+                distance = data.distance or 2.0
+            }
+        )
+    end
+
+    return nil
+end
+
+local function RemoveZone(id)
+    if UsingOxTarget then
+        exports.ox_target:removeZone(id)
+    elseif UsingQBTarget then
+        exports['qb-target']:RemoveZone(id)
+    end
+end
+
+--------------------------------------------------
+-- HOTEL NPC INTERACTION
+--------------------------------------------------
+
+function RegisterHotelNPC(hotel)
+    if not hotel or not hotel.npc then return end
+
+    local zone = {
+        name = "hotel_npc_" .. hotel.id,
+
+        coords = vector3(
+            hotel.npc.coords.x,
+            hotel.npc.coords.y,
+            hotel.npc.coords.z
+        ),
+
+        size = vec3(1.5, 1.5, 2.0),
+
+        heading = hotel.npc.coords.w,
+
+        debug = false,
+
+        options = {
+            {
+                name = "hotel_open_" .. hotel.id,
+                icon = "fa-solid fa-hotel",
+                label = "Speak to Reception",
+
+                onSelect = function()
+                    TriggerEvent("hotel:openMenu", hotel.id)
+                end
+            }
+        }
+    }
+
+    local zoneId = AddTargetZone(zone)
+
+    RegisteredZones["npc_" .. hotel.id] = zoneId
+end
+
+--------------------------------------------------
+-- ROOM STASH INTERACTION
+--------------------------------------------------
+
+function RegisterRoomStash(hotelId, room)
+    local zone = {
+        name = "hotel_stash_" .. hotelId .. "_" .. room.id,
+
+        coords = room.stash,
+
+        size = vec3(1.0, 1.0, 1.0),
+
+        options = {
+            {
+                name = "open_stash_" .. room.id,
+                icon = "fa-solid fa-box",
+                label = "Open Room Stash",
+
+                onSelect = function()
+                    TriggerEvent("hotel:openStash", hotelId, room.id)
+                end
+            }
+        }
+    }
+
+    local zoneId = AddTargetZone(zone)
+
+    RegisteredZones["stash_" .. hotelId .. "_" .. room.id] = zoneId
+end
+
+------------------------------------------------
