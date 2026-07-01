cfbc46e728c70b1684274272dd5db3859b91f979
Robert Collins <124530790+UselessRobUK@users.noreply.github.com>
2026-07-01 03:06:26 +0100

    Update rooms.lua
    Rooms for Peak Towers added on levels 11, 12, 13 & 14

--- configs/shared/rooms.lua
+++ configs/shared/rooms.lua
@@ -32,18 +32,243 @@ return {
     peak = {
         {
             id       = 1101,
-            label    = "Room 1001",
+            label    = "1101",
             price    = 250,
             duration = 24,
 
             door = {
                 id      = "peak_1101",
-                coords  = vector3(52.9309, -940.4468, 193.4848),
-                heading = 228.6677,
+                coords  = vector3(52.9278, -940.4611, 193.4392),
+                heading = 230,
                 model   = 1304959123,
             },
-            stash    = vector3(74.5612, -946.9492, 193.4050),
-            wardrobe = vector3(52.0965, -946.8171, 193.8213),
+            stash    = vector3(52.8582, -940.5440, 193.5574),
+            wardrobe = vector3(49.0472, -936.6489, 193.3471),
+        },
+        {
+            id       = 1102,
+            label    = "1102",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1102",
+                coords  = vector3(49.0365, -936.6617, 193.2591),
+                heading = 230.0001,
+                model   = 1304959123,
+            },
+            stash    = vector3(38.7437, -916.9662, 193.6010),
+            wardrobe = vector3(43.5936, -939.7114, 193.4553),
+        },
+        {
+            id       = 1103,
+            label    = "1103",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1103",
+                coords  = vector3(65.8224, -925.0939, 193.4656),
+                heading = 50,
+                model   = 1304959123,
+            },
+            stash    = vector3(76.0873, -944.9849, 193.6714),
+            wardrobe = vector3(71.3357, -922.4139, 193.5320),
+        },
+        {
+            id       = 1104,
+            label    = "1104",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1104",
+                coords  = vector3(61.9553, -921.2642, 193.3037),
+                heading = 50.1215,
+                model   = 1304959123,
+            },
+            stash    = vector3(40.3748, -915.0078, 193.3176),
+            wardrobe = vector3(63.4977, -915.6739, 193.3171),
+        },
+        {
+            id       = 1201,
+            label    = "1201",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1201",
+                coords  = vector3(53.0601, -940.3035, 198.1083),
+                heading = 230,
+                model   = 1304959123,
+            },
+            stash    = vector3(67.7773, -940.7642, 197.9327),
+            wardrobe = vector3(51.0943, -946.0052, 198.1896),
+        },
+        {
+            id       = 1202,
+            label    = "1202",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1202",
+                coords  = vector3(48.9733, -936.7371, 198.2337),
+                heading = 230.0001,
+                model   = 1304959123,
+            },
+            stash    = vector3(48.5828, -924.6581, 198.0030),
+            wardrobe = vector3(43.5225, -939.6518, 198.2132),
+        },
+        {
+            id       = 1203,
+            label    = "1203",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1203",
+                coords  = vector3(66.0222, -924.8558, 197.9470),
+                heading = 50,
+                model   = 1304959123,
+            },
+            stash    = vector3(66.2958, -937.2615, 197.8487),
+            wardrobe = vector3(71.1917, -922.1298, 198.2041),
+        },
+        {
+            id       = 1204,
+            label    = "1204",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1204",
+                coords  = vector3(61.9316, -921.2940, 198.2054),
+                heading = 50.0001,
+                model   = 1304959123,
+            },
+            stash    = vector3(49.8095, -923.4279, 198.1140),
+            wardrobe = vector3(63.9835, -916.0815, 198.2934),
+        },
+        {
+            id       = 1301,
+            label    = "1301",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1301",
+                coords  = vector3(52.8515, -940.5480, 202.4472),
+                heading = 229.7760,
+                model   = 1304959123,
+            },
+            stash    = vector3(65.2767, -938.6659, 202.3251),
+            wardrobe = vector3(51.1176, -946.0248, 202.7417),
+        },
+        {
+            id       = 1302,
+            label    = "1302",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1302",
+                coords  = vector3(48.9687, -936.7410, 202.5936),
+                heading = 229.7977,
+                model   = 1304959123,
+            },
+            stash    = vector3(48.5784, -924.6544, 202.4082),
+            wardrobe = vector3(43.7375, -939.8323, 202.6719),
+        },
+        {
+            id       = 1303,
+            label    = "1303",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1303",
+                coords  = vector3(65.8826, -925.0222, 202.5587),
+                heading = 50,
+                model   = 1304959123,
+            },
+            stash    = vector3(66.1743, -937.1595, 202.1615),
+            wardrobe = vector3(70.9642, -921.9389, 202.7947),
+        },
+        {
+            id       = 1304,
+            label    = "1304",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1304",
+                coords  = vector3(62.0684, -921.1310, 202.5182),
+                heading = 50.0001,
+                model   = 1304959123,
+            },
+            stash    = vector3(49.5305, -923.1937, 202.4342),
+            wardrobe = vector3(63.6774, -915.8247, 202.7253),
+        },
+        {
+            id       = 1401,
+            label    = "1401",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1401",
+                coords  = vector3(53.0203, -940.3509, 207.1466),
+                heading = 230,
+                model   = 1304959123,
+            },
+            stash    = vector3(65.2541, -938.7362, 206.4943),
+            wardrobe = vector3(51.5331, -946.0328, 207.1920),
+        },
+        {
+            id       = 1402,
+            label    = "1402",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1402",
+                coords  = vector3(48.8558, -936.8771, 206.9371),
+                heading = 230.0001,
+                model   = 1304959123,
+            },
+            stash    = vector3(48.6233, -924.6921, 206.4629),
+            wardrobe = vector3(43.5556, -939.6796, 207.1362),
+        },
+        {
+            id       = 1403,
+            label    = "1403",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1403",
+                coords  = vector3(65.7486, -925.1819, 207.0278),
+                heading = 50,
+                model   = 1304959123,
+            },
+            stash    = vector3(66.3921, -937.3423, 206.8089),
+            wardrobe = vector3(71.2518, -922.5328, 207.2695),
+        },
+        {
+            id       = 1404,
+            label    = "1404",
+            price    = 250,
+            duration = 24,
+
+            door = {
+                id      = "peak_1404",
+                coords  = vector3(61.8787, -921.3419, 207.0501),
+                heading = 50.7671,
+                model   = 1304959123,
+            },
+            stash    = vector3(49.5925, -923.2458, 206.5977),
+            wardrobe = vector3(63.2700, -915.8085, 207.1707),
         },
     },
 }
